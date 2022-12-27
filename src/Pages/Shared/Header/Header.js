37ec577700a7769bce9091cf7c76e3b1852824b4
Noob-H4cker <alsamiul123@gmail.com>
2022-12-27 14:34:40 +0600

    navbar toggle background fixed

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,20 +9,36 @@ import icon3 from '../../../Assets/img/nav-icon3.svg';
 import './Header.css';
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const handleToggleClick = () => {
+    setOpen(!open);
+  };
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" variant="dark">
+      <Navbar collapseOnSelect expand="lg" variant="dark" className={open ? 'open ' : ''}>
         <Container className="scrolled">
           <Navbar.Brand href="#" className="fontSize">
             𝕸𝖔𝖍𝖆𝖒𝖒𝖆𝖉 𝕬𝖑 𝕾𝖆𝖒𝖎𝖚𝖑
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle
+            onClick={handleToggleClick}
+            className="toggleBar"
+            aria-controls="responsive-navbar-nav"
+          />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="d-lg-flex justify-content-center m-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#contact">Contact Me</Nav.Link>
+              <Nav.Link href="#home" onClick={handleToggleClick}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="#skills" onClick={handleToggleClick}>
+                Skills
+              </Nav.Link>
+              <Nav.Link href="#projects" onClick={handleToggleClick}>
+                Projects
+              </Nav.Link>
+              <Nav.Link href="#contact" onClick={handleToggleClick}>
+                Contact Me
+              </Nav.Link>
             </Nav>
             <Nav>
               <span className="navbar-text">
