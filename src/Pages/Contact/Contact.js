4b945c596c24a01cbd8367fbff6fe50155eab25b
Noob-H4cker <alsamiul123@gmail.com>
2022-12-28 20:44:14 +0600

    contact form completed

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import contactImg from '../../Assets/img/contact-img.svg';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div>
+      <section className="contact" id="connect">
+        <Container>
+          <Row className="align-items-center">
+            <Col size={12} md={6}>
+              <img className="" src={contactImg} alt="Contact Us" />
+            </Col>
+            <Col size={12} md={6}>
+              <div className="p-4">
+                <h2 className="get-in-touch">Get In Touch</h2>
+                <form>
+                  <Row>
+                    <Col size={12} sm={6} className="px-2">
+                      <input type="text" placeholder="First Name" />
+                    </Col>
+                    <Col size={12} sm={6} className="px-2">
+                      <input type="text" placeholder="Last Name" />
+                    </Col>
+                    <Col size={12} sm={6} className="px-2">
+                      <input type="email" placeholder="Email Address" />
+                    </Col>
+                    <Col size={12} sm={6} className="px-2">
+                      <input type="tel" placeholder="Phone No." />
+                    </Col>
+                    <Col size={12} className="px-2">
+                      <textarea rows="4" placeholder="Message"></textarea>
+                      <button type="submit">
+                        <span></span>
+                      </button>
+                    </Col>
+                  </Row>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,32 +2,38 @@
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import headerImg from '../../Assets/img/header-img.svg';
+import Contact from '../Contact/Contact';
 import './Home.css';
 const Home = () => {
   return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="align-items-center justify-content-between">
-          <Col xs={12} md={6} xl={6} className="banner-padding">
-            <h1 className="tagline">Welcome to My Portfolio</h1>
-            <h2 className="margin">
-              {`Hi, This is Mohammad Al Samiul`} <br />
-              <span className="h4">Front-End Developer</span>{' '}
-            </h2>
-            <p className="mt-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eligendi recusandae,
-              natus ullam aliquam temporibus! Repellendus, fuga explicabo ullam necessitatibus.
-            </p>
-            <Button className="margin" variant="outline-light">
-              Let's connect
-            </Button>
-          </Col>
-          <Col xs={12} md={6} xl={4}>
-            <img className="img-width" src={headerImg} alt="Header Img" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <div>
+      <section className="banner" id="home">
+        <Container>
+          <Row className="align-items-center justify-content-between">
+            <Col xs={12} md={6} xl={6} className="banner-padding">
+              <h1 className="tagline">Welcome to My Portfolio</h1>
+              <h2 className="margin">
+                {`Hi, This is Mohammad Al Samiul`} <br />
+                <span className="h4">Front-End Developer</span>{' '}
+              </h2>
+              <p className="mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eligendi recusandae,
+                natus ullam aliquam temporibus! Repellendus, fuga explicabo ullam necessitatibus.
+              </p>
+              <Button className="margin" variant="outline-light">
+                Let's connect
+              </Button>
+            </Col>
+            <Col xs={12} md={6} xl={4}>
+              <img className="img-width" src={headerImg} alt="Header Img" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <div>
+        <Contact />
+      </div>
+    </div>
   );
 };
 
